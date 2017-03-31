a94eb5a89e0316ca4935256c42afd2223c622d06
Jakub Zalas <jakub@zalas.pl>
2017-03-31 21:15:27 +0100

    Implement the DecodingBoard

--- app/mastermind/DecodingBoard.scala
+++ app/mastermind/DecodingBoard.scala
@@ -1,4 +1,12 @@
 package mastermind
 
 class DecodingBoard(val gameUuid: GameUuid, private val secretCode: Code, val numberOfAttempts: Int) {
+  private var guesses = Seq.empty[Guess]
+
+  def tryCode(code: Code): Guess = {
+    guesses = guesses :+ new Guess(secretCode, code)
+    guesses.last
+  }
+
+  def lastGuess(): Guess = guesses.last
 }
---
+++ test/mastermind/DecodingBoardSpec.scala
@@ -0,0 +1,28 @@
+package mastermind
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.{FlatSpec, Matchers}
+
+@RunWith(classOf[JUnitRunner])
+class DecodingBoardSpec extends FlatSpec with Matchers {
+  "tryCode" should "return a new guess" in {
+    val gameUuid = GameUuid()
+    val secretCode = Code("Red Green Blue Yellow")
+    val numberOfAttempts = 6
+    val board = new DecodingBoard(gameUuid, secretCode, numberOfAttempts)
+    val guess = board.tryCode(Code("Blue Red Green Green"))
+
+    guess.secretCode should have('pegs (Code("Red Green Blue Yellow").pegs))
+    guess.guessCode should have('pegs (Code("Blue Red Green Green").pegs))
+  }
+
+  "lastGuess" should "return the most recent guess" in {
+    val board = new DecodingBoard(GameUuid(), Code("Red Green Blue Yellow"), 6)
+
+    board.tryCode(Code("Blue Red Green Green"))
+    val guess = board.tryCode(Code("Red Red Blue Blue"))
+
+    board.lastGuess() should be(guess)
+  }
+}
