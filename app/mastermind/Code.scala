314096236c98e1913a6e4b67cae809892c70b1bc
Jakub Zalas <jakub@zalas.pl>
2017-03-31 21:11:23 +0100

    Improve readability

--- app/mastermind/Code.scala
+++ app/mastermind/Code.scala
@@ -8,11 +8,21 @@ object Code {
 
 class Code(val pegs: List[CodePeg.Value]) {
   def colourHits(code: Code): Int = {
-    val filteredPegs = pegs.zip(code.pegs).filter(pegTuple => pegTuple._1 != pegTuple._2).unzip
-    val left = filteredPegs._1.groupBy(peg => peg).mapValues(_.size)
-    val right = filteredPegs._2.groupBy(peg => peg).mapValues(_.size)
+    def removeExactHits(pegs: List[CodePeg.Value]): (List[CodePeg.Value], List[CodePeg.Value]) = {
+      pegs.zip(code.pegs).filter(pegTuple => pegTuple._1 != pegTuple._2).unzip
+    }
 
-    left.foldLeft(0)((hits, pegCount) => hits + math.min(pegCount._2, right.getOrElse(pegCount._1, 0)))
+    def countColours(pegs: List[CodePeg.Value]): Map[CodePeg.Value, Int] = {
+      pegs.groupBy(peg => peg).mapValues(_.size)
+    }
+
+    def calculateHits(left: Map[CodePeg.Value, Int], right: Map[CodePeg.Value, Int]): Int = {
+      left.foldLeft(0)((hits, pegCount) => hits + math.min(pegCount._2, right.getOrElse(pegCount._1, 0)))
+    }
+
+    val filteredPegs = removeExactHits(pegs)
+
+    calculateHits(countColours(filteredPegs._1), countColours(filteredPegs._2))
   }
 
   def exactHits(code: Code): Int = {
