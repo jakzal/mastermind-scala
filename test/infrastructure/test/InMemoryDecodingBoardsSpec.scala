a88b95c59db2ff72b8f4e3278d52f2269b217291
Jakub Zalas <jakub@zalas.pl>
2017-06-10 00:13:30 +0100

    Remove an unnecessary trait

--- test/infrastructure/test/InMemoryDecodingBoardsSpec.scala
+++ test/infrastructure/test/InMemoryDecodingBoardsSpec.scala
@@ -2,11 +2,9 @@ package infrastructure.test
 
 import mastermind.{DecodingBoards, DecodingBoardsSpec}
 import org.junit.runner.RunWith
-import org.scalatest.BeforeAndAfter
 import org.scalatest.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
-class InMemoryDecodingBoardsSpec extends DecodingBoardsSpec with BeforeAndAfter {
-
+class InMemoryDecodingBoardsSpec extends DecodingBoardsSpec {
   def createDecodingBoards: DecodingBoards = new InMemoryDecodingBoards()
 }
